11ab684316c66e8b16fcd08006d2ef70ce3aeac1
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-02-14 18:59:26 -0300

    adiciona classe endereco p conversao de cep

---
+++ AdaFood/Domain/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace AdaFood.Domain
+{
+    public class Endereco
+    {
+        public string cep { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+        public string neighborhood { get; set; }
+        public string street { get; set; }
+        public string service { get; set; }
+        public int? number { get; set; }
+    }
+}
